a199171f08f2382928cdfc3cc8805836607f5c26
entwoane <antoine.musci@gmail.com>
2024-10-22 11:55:23 +0200

    Bettyyyy

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
 */
 int main(void)
 {
-	char a = 'a';
-	char b = 'A';
+	char a;
 
-	for (char a = 'a'; a <= 'z'; a++)
-{
-	putchar(a);
-}
-	for (char b <= 'Z'; b++)
-{
-	putchar(b);
-}
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
 return (0);
 }
