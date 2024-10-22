205b9624b9ccb8731ec8ed339a59fefd2210c89f
entwoane <antoine.musci@gmail.com>
2024-10-22 14:11:10 +0200

    Betty je sais compter

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Print
+ * Return: Always 0 if Success.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
+
