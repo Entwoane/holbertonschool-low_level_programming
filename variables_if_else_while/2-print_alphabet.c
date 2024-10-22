bd4866c53db7fb3690b7761be63701cf8fd8152c
entwoane <antoine.musci@gmail.com>
2024-10-22 10:02:20 +0200

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: Print the alphabet in lowercase
+ * Return: Always 0 if Success.
+ */
+int main(void)
+{
+	int a;
+	for ( a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+return (0);
+}
