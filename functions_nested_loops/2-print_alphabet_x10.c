15a98337d9d85896842ca2add0ccb34a18aecc42
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-23 20:15:39 +0200

    Alphabet 10x

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,18 @@
 void print_alphabet_x10(void)
 {
 	int n;
+	char a;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		print_alphabet();
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
 	}
 }
+
+
 
 
