06259435b5e22f29fa0c0139d75129f515a171b5
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-24 16:46:09 +0200

    print sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - entry point
+ *@n : number
+ *
+ *Return: On success 1.
+ * On error, 0 is returned
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
