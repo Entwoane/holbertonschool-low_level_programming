5624eae1da178dee6eb135b1479e4c4e2125f5b1
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-25 15:33:42 +0200

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+* add - entry point
+* @a: - integer
+* @b: integer
+*
+*Return: a + b result
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+* times_table - entry point
+*
+*
+*Return: On success 0.
+*/
+void times_table(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		
+	}
+
+}
+
