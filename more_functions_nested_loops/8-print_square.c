84aa65500c4a1c303a4c135323dd31a7b5925ebb
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-28 20:02:53 +0100

    #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * print_diagonal - entry point
+ *@size: size of the square
+ *
+ * return (0) success
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
