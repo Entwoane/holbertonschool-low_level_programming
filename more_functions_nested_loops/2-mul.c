032e2a264a695164d6f4098181c96320ebf09cb8
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-28 10:27:20 +0100

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * mul - entry point
+ *@a: char to multiply
+ *@b: char to multiplay
+ *
+ *Return: On success 1.
+ * On error, 0 is returned
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
