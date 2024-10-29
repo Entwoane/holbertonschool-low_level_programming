c861ed37113af6d6b3adbb067c79bbfe8e1075cd
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-29 16:49:47 +0100

    puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _puts - entry point
+ *@str: pointer
+ *
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
