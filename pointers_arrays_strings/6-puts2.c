72a50d80ca17f95c5dafd49ef46c811f7a914552
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-30 16:14:47 +0100

    Lorem

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,11 @@
 void puts2(char *str)
 {
 	int i = 0;
-	int counter = 0;
-
-	if (counter % 2 == 0)
-	_putchar(str[i]);
 
 	while (str[i] != '\0')
 	{
-	_putchar(str[i += 2]);
+	_putchar(str[i]);
+	i += 2;
 	}
 
 	_putchar('\n');
