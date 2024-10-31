adefe390b0c7277f2327f654dd5dc52e57b1a583
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-10-31 14:43:05 +0100

    Reverse

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,25 @@
  */
 void rev_string(char *s)
 {
-	
-}
+	int count = 0;
+	int i;
+	char v[500];
 
+
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (i = 0; i < count; i++)
+	{
+		v[i] = s[count - 1 - i];
+
+	}
+		v[count] = '\0';
+
+	for (i = 0; i <= count; i++)
+	{
+		s[i] = v[i];
+	}
+}
