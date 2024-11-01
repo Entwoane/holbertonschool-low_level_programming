863b9ce2a1bb92dd3bc3836cda131fc2b195d905
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-11-01 14:55:36 +0100

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,6 +8,26 @@
  */
 void puts_half(char *str)
 {
-	
+	int count = 0;
+	int i, n;
+
+	while (str[count] != '\0')
+	count++;
+
+	if (count % 2 == 0)
+	{
+		for (i = count / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (count % 2)
+	{
+		for (n = (count - 1) / 2; n < count - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
 
 }
