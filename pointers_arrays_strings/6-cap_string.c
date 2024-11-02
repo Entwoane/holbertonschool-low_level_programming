60cb15672ab953497c026444a5ecdd955e86b263
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-11-02 13:24:03 +0100

    CaPitalize

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 || i == '-')
+		if (i == 0)
 		{
 			if ((str[i] >= 'a' && str[i] <= 'z'))
 			{
@@ -30,6 +30,13 @@ char *cap_string(char *str)
 		if (str[i] == '\t' || str[i] == '\n')
 		{
 			i++;
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		if (str[i] == '-')
+		{
 			if ((str[i] >= 'a' && str[i] <= 'z'))
 			{
 				str[i] = str[i] - 32;
