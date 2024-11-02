9172c76415042dac9e13c8c2b420b3a4199c215c
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-11-02 13:10:48 +0100

    capitalize maybe

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include"main.h"
+/**
+ * cap_string - entry point
+ *@str : char to convert
+ *
+ *Return: On success 1.
+ * On error, 0 is returned
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		if (str[i] == ' ' || str[i] == '.')
+		{
+			i++;
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		if (str[i] == '\t' || str[i] == '\n')
+		{
+			i++;
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+
+	return (str);
+}
