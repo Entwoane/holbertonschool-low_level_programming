c3a217133529a243ffda6f40f0e79c46560e4364
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-11-02 10:39:30 +0100

    cmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+/**
+ *_strcmp - entry point
+ *@s1: comparaison 1
+ *@s2: comparaison 2
+ *
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+
+	return ((*s1 > *s2) - (*s1 < *s2));
+}
