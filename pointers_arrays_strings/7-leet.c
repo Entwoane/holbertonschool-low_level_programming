3123059d53c56f1a6348e9f5996a269bae2ce220
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-11-03 09:15:13 +0100

    1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ * leet - entry point
+ *@s: pointer
+ *
+ *
+ * Return: Always 0
+ */
+char *leet(char *s)
+{
+	char *r = s;
+	char a[] = { 'a', 'e', 'o', 't', 'l' };
+	char n[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
+
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == a[i] - 32)
+			{
+				*s = n[i] + '0';
+			}
+		}
+		s++;
+	}
+	return (r);
+}
