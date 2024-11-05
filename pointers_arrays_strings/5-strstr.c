487110bad42ad4ba7489b078460f791f408b26ed
Entwoane <entwoane@LAPTOP-T8UJVOCK.>
2024-11-05 21:19:03 +0100

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -8,13 +8,30 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
+	const char *h;
+	const char *n;
+
+
+	if (*needle == '\0')
+		return ((char *)haystack);
+
+	while (*haystack != '\0')
 	{
-		if (*haystack == *needle)
+		h = haystack;
+		n = needle;
+
+		while (*h == *n && *n != '\0')
 		{
-			return (haystack);
+			h++;
+			n++;
 		}
+
+		if (*n == '\0')
+			return ((char *)haystack);
+
 		haystack++;
 	}
+
 	return (0);
 }
+
