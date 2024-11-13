6ffbd9649b4412721a56a6d1ecc70c097617a9a7
Entwoane <antoine.musci@gmail.com>
2024-11-13 15:55:59 +0100

    Woof woof

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: doggo's owner
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
