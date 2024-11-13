dd0407a68f343df472a56cf30ed9cd01402589d2
Entwoane <antoine.musci@gmail.com>
2024-11-13 15:28:26 +0100

    doggo

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Structure containing dog's infos
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
