2bf27190734533ff533595cc3568fae23c663880
Entwoane <antoine.musci@gmail.com>
2024-11-13 16:37:56 +0100

    dog_t

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * Description: Structure containing dog's infos
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
