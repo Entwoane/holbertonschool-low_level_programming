98d92c479e1764b16807040631aaa22053ba6445
Entwoane <antoine.musci@gmail.com>
2024-11-13 14:26:40 +0100

    hotfix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,15 +12,17 @@ int *array_range(int min, int max)
 {
 	int *arr;
 	int i;
+	int size;
 
 	if (min > max)
 		return (NULL);
 
-	arr = (int *) malloc((max - min + 1) * sizeof(int));
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= max + min; i++)
+	for (i = 0; i <= size; i++)
 		arr[i] = min + i;
 
 	return (arr);
