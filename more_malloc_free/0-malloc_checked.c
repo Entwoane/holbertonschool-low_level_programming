816e1f3dde1dac02d99ef936c0fe1fe39bfd3863
Entwoane <antoine.musci@gmail.com>
2024-11-13 09:42:49 +0100

    trust no one

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - entry point
+ * @b: unsigned int
+ *
+ * Return: a pointer to the allocated memory
+ * Return 98 if fail
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(*ptr) * b);
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(98);
+	}
+
+	return (ptr);
+}
