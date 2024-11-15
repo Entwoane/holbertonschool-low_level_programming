833d620833062cf3b9be2ce0a3c39ff7aba61ebb
Entwoane <antoine.musci@gmail.com>
2024-11-15 11:52:32 +0100

    dont know

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
+
 #include "dog.h"
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * new_dog - function that creates a new dog
@@ -12,34 +13,44 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *namecpy;
-	char *ownercpy;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
+	dog_t *new_dog;
+	int name_len = 0;
+	int owner_len = 0;
+	int i = 0;
+
+
+	while (name[name_len] != '\0')
+		name_len++;
+
+	while (owner[owner_len] != '\0')
+		owner_len++;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 
-	namecpy = malloc(strlen(name) + 1);
-		if (namecpy == NULL)
-		{
-			free(d);
-			return (NULL);
-		}
-	strcpy(namecpy, name);
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
 
-	ownercpy = malloc(strlen(owner) + 1);
-		if (ownercpy == NULL)
-		{
-			free(namecpy);
-			free(d);
-			return (NULL);
-		}
-	strcpy(ownercpy, owner);
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
 
-	d->name = namecpy;
-	d->age = age;
-	d->owner = ownercpy;
+	for (i = 0; i < name_len; i++)
+		new_dog->name[i] = name[i];
 
-	return (d);
+	for (i = 0; i < owner_len; i++)
+		new_dog->owner[i] = owner[i];
+
+	new_dog->age = age;
+
+	return (new_dog);
 }
