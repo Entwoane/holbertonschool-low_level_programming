23038ccc3ebf7599b32ad4f2fdbeca97da38ce80
Entwoane <antoine.musci@gmail.com>
2024-11-20 13:46:10 +0100

    hotfix

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -14,16 +14,17 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum;
 
+	if (n == 0)
+	{
+		return (0);
+	}
+
 	va_start(ap, n);
 
 	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
-	}
-	if (n == 0)
-	{
-		return (0);
 	}
 		va_end(ap);
 
