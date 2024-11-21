9444c07f0b91389f4f0b6c81b09727195e9f69fa
Entwoane <antoine.musci@gmail.com>
2024-11-21 18:56:21 +0100

    function that prints anything passed to it. Include char, integer, float and string

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,100 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_char - function that prints a character
+ * @args: argument list
+ *
+ * Return: char
+ */
+void print_char(va_list *args)
+{
+	printf("%c", va_arg(*args, int));
+}
+/**
+ * print_int - function that prints an integer
+ * @args: argument list
+ *
+ * Return: int
+ */
+void print_int(va_list *args)
+{
+	printf("%i", va_arg(*args, int));
+}
+/**
+ * print_float - function that prints a float
+ * @args: argument list
+ *
+ * Return: float
+ */
+void print_float(va_list *args)
+{
+	printf("%f", va_arg(*args, double));
+}
+/**
+ * print_string - function that prints a string
+ * @args: argument list
+ *
+ * Return: string
+ */
+void print_string(va_list *args)
+{
+	char *str = va_arg(*args, char *);
+
+	if (str == NULL)
+	{
+	printf("(nil)");
+	return;
+	}
+	printf("%s", str);
+}
+/**
+ * print_all - Function that prints anything
+ * @format: format to print
+ *
+ * Return: format
+ */
+void print_all(const char * const format, ...)
+{
+
+	form f[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
+
+	int i = 0, j;
+	char *separator = "";
+
+	va_list args;
+
+	va_start(args, format);
+	if (format == NULL)
+	{
+		return;
+	}
+
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (f[j].str)
+		{
+			if (f[j].str == format[i])
+			{
+				printf("%s", separator);
+				f[j].print(&args);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+
+}
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,22 @@
 #ifndef VAR_FUNC_H
 #define VAR_FUNC_H
+#include <stdarg.h>
+/**
+ * struct formatter - Struct
+ *
+ * @str: string for print_string function
+ * @print: The function associated to va_list
+ */
+typedef struct formatter
+{
+	char str;
+	void (*print)(va_list *);
+} form;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
