497d11d35c32b7c3c5c25a2c926e6b16d4a7ea73
Entwoane <antoine.musci@gmail.com>
2024-12-04 14:09:04 +0100

    Update on the memory allocation, correct initialization of the new node, verifying if the list is empty and general code update

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,6 +16,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *end;
 
 	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
 
 	newNode->n = n;
 	newNode->next = NULL;
