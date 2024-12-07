841b9f2918db62eb12585a3de367ae58ef8a06dd
Entwoane <antoine.musci@gmail.com>
2024-12-07 22:29:36 +0100

    Now includes every prototypes used

--- file_io/main.h
+++ file_io/main.h
@@ -11,5 +11,9 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_exit(int code, const char *msg, const char *file, int fd);
+void close_file(int fd);
+void copy_file(int fd_from, int fd_to, const char *file_to);
+int main(int argc, char *argv[]);
 
 #endif
