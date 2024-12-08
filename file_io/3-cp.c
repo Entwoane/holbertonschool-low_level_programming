99c2f11151455619a0b61804c1787e236bb04f09
Entwoane <antoine.musci@gmail.com>
2024-12-08 20:39:29 +0100

    Betty is not happy and I dont know why

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,7 +11,6 @@ void error_exit(int code, const char *msg, const char *arg)
 	dprintf(STDERR_FILENO, msg, arg);
 	exit(code);
 }
-
 /**
  * main - Copies content of a file to another file
  * @argc: Number of arguments
@@ -25,18 +24,15 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		error_exit(97, "Usage: cp file_from file_to\n", NULL);
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
-
 	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		wr = write(fd_to, buffer, rd);
@@ -47,7 +43,6 @@ int main(int argc, char *argv[])
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
 	}
-
 	if (rd == -1)
 	{
 		close(fd_from);
@@ -57,9 +52,7 @@ int main(int argc, char *argv[])
 
 	if (close(fd_from) == -1)
 		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
-
 	if (close(fd_to) == -1)
 		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
-
 	return (0);
 }
