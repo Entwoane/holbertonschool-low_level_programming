b5712cb0e4d4fbc97b554d4f9dbb010548ad6206
Entwoane <antoine.musci@gmail.com>
2024-12-08 20:30:24 +0100

    Header with all the libraries and prototypes used

--- file_io/main.h
+++ file_io/main.h
@@ -11,9 +11,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_exit(int code, const char *msg, const char *file, int fd);
-void close_file(int fd);
-void copy_file(int fd_from, int fd_to, const char *file_to);
+void error_exit(int code, const char *msg, const char *arg);
 int main(int argc, char *argv[]);
 
 #endif
