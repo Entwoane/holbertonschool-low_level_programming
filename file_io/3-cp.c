df2f245ab857cc98c0eb25d975502d08dc289c5b
Entwoane <antoine.musci@gmail.com>
2024-12-08 22:27:02 +0100

    Definie buffer size, allocate memory to buffer and use of a do/while loop

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#define BUFFER_SIZE 1024
 /**
  * error_exit - Prints error message and exits
  * @code: Exit code
@@ -19,41 +20,42 @@ void error_exit(int code, const char *msg, const char *arg)
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, wr;
-	ssize_t rd;
-	char buffer[BUFFER_SIZE];
+	int fd_from, fd_to, rd, wr;
+	char *buffer;
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", NULL);
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
 	{
-		close(fd_from);
+		error_exit(97, "Usage: cp file_from file_to\n", NULL);
+	}
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
-	while ((rd = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		wr = write(fd_to, buffer, rd);
-		if (wr != rd)
+	fd_from = open(argv[1], O_RDONLY);
+	rd = read(fd_from, buffer, 1024);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	do {
+		if (fd_from == -1 || rd == -1)
 		{
-			close(fd_from);
-			close(fd_to);
+			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		}
+		wr = write(fd_to, buffer, rd);
+		if (fd_to == -1 || wr == -1)
+		{
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 		}
-	}
-	if (rd == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
-
+		rd = read(fd_from, buffer, 1024);
+		fd_to = open(argv[2], O_WRONLY | O_APPEND);
+	} while (rd > 0);
+	free(buffer);
 	if (close(fd_from) == -1)
+	{
 		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
+	}
 	if (close(fd_to) == -1)
+	{
 		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
+	}
 	return (0);
 }
